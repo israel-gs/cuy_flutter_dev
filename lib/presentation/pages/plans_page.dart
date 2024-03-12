c743ea9e1e528ada6ba89bc68c8f3e4229cde03b
israel-gs <israelgs83@gmail.com>
2024-03-12 01:56:49 -0500

    add plans page

---
+++ lib/presentation/pages/plans_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PlansPage extends StatelessWidget {
+  const PlansPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 38),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [],
+      ),
+    );
+  }
+}
