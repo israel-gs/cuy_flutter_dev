1514bef612e5059baf7f93f64a0b6ee4b7380f6c
israel-gs <israelgs83@gmail.com>
2024-03-12 05:11:50 -0500

    remove unnecesary code

--- test/presentation/bloc/plans_bloc_test.dart
+++ test/presentation/bloc/plans_bloc_test.dart
@@ -1,5 +1,4 @@
 import 'package:bloc_test/bloc_test.dart';
-import 'package:cuy_flutter_dev/domain/entities/plans_entity.dart';
 import 'package:cuy_flutter_dev/presentation/bloc/plans/plans_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
@@ -17,84 +16,9 @@ void main() {
     plansBloc = PlansBloc(mockGetActivePlansUseCase, mockGetTokenUseCase);
   });
 
-  const authorization =
-      'sys_sk_test_2sIsFGVeXtSRqMOKpVQ926TMKFAbOEZyKg30WqNtjQkl5Ko';
-
-  const List<ListElementEntity> testList = [
-    ListElementEntity(
-      id: 1,
-      bundleId: "12226",
-      name: "1 GB",
-      description: "Pkg_Data_1_30",
-      quantity: 1,
-      quantityReal: null,
-      price: 1,
-      duration: 30,
-      isSelected: false,
-      isUnlimited: false,
-      pricePlanId: "110011",
-      isPopular: false,
-      isRecommended: false,
-      repurchase: false,
-      isRollover: false,
-      name2: "",
-      name3: "",
-      hasFreeApps: false,
-      hasFacebookFull: false,
-      hasInstagramFull: false,
-      hasFacebookPhoto: false,
-      hasInstagramPhoto: false,
-      facebookFullDesc: "",
-      groupName: "",
-      nameByte: "",
-      nameSecond: "",
-      nameSms: "",
-      isUnlimitedByte: false,
-      isUnlimitedSecond: false,
-      isUnlimitedSms: false,
-      quantityByte: 0,
-      quantitySecond: 0,
-      quantitySms: 0,
-      bundleIds: "",
-      mediumSpeed: "",
-      mediumSpeedDescription: "",
-      whiteBrand: 1,
-      freeApps: [],
-      migrations: [],
-    )
-  ];
-
-  const testPlansEntity = PlansEntity(
-    code: "200",
-    status: "success",
-    message: "Consulta realizado correctamente",
-    list: testList,
-    appListFree: [],
-  );
-
   test('initial state should be PlansInitial', () {
     expect(plansBloc.state, equals(PlansInitial()));
   });
-
-  /* blocTest<PlansBloc, PlansState>(
-    'should emit [PlansLoading, PlansSuccess] when data is gotten successfully',
-    build: () {
-      when(mockGetTokenUseCase()).thenAnswer(
-        (_) async {
-          return Future.value(authorization);
-        },
-      );
-      when(mockGetActivePlansUseCase(authorization: authorization)).thenAnswer(
-        (_) async => const Right(testPlansEntity),
-      );
-      return plansBloc;
-    },
-    act: (bloc) async => bloc.add(
-      const PlansStarted(),
-    ),
-    wait: const Duration(milliseconds: 500),
-    expect: () => [PlansLoading(), const PlansSuccess(testList)],
-  ); */
 
   blocTest<PlansBloc, PlansState>(
     'should emit [PlansLoading, PlansFailure] when token is null',
