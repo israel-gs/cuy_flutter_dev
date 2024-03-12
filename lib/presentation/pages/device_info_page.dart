275fc4e6eba643aac7328b6057b87c3c403f3848
israel-gs <israelgs83@gmail.com>
2024-03-12 01:56:44 -0500

    add device info page

---
+++ lib/presentation/pages/device_info_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DeviceInfoPage extends StatelessWidget {
+  const DeviceInfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 38),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [],
+      ),
+    );
+  }
+}
