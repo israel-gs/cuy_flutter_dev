4eb4b3f382ae5502b98ebaedd81f10f42681ec26
israel-gs <israelgs83@gmail.com>
2024-03-12 01:57:16 -0500

    add configs to main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
+import 'package:cuy_flutter_dev/config/routes/routes.dart';
+import 'package:cuy_flutter_dev/config/theme/theme.dart';
 import 'package:cuy_flutter_dev/injection_container.dart';
+import 'package:cuy_flutter_dev/presentation/bloc/auth/auth_bloc.dart';
+import 'package:cuy_flutter_dev/presentation/bloc/login/login_bloc.dart';
+import 'package:cuy_flutter_dev/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   await init();
@@ -11,60 +17,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => sl<AuthBloc>()
+            ..add(
+              const AuthStarted(
+                email: 'sys@cuy.pe',
+                password: '123456',
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        BlocProvider(create: (context) => sl<LoginBloc>()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: theme(),
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: AppRoutes.onGenerateRoutes,
+        home: const LoginPage(),
       ),
     );
   }
