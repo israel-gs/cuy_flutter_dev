afce425ec2bc36fbcbf5b3405c6358114d10cc71
israel-gs <israelgs83@gmail.com>
2024-03-12 01:55:32 -0500

    add fromJson and toJson functions to model

--- lib/data/models/auth_request_model.dart
+++ lib/data/models/auth_request_model.dart
@@ -6,4 +6,18 @@ class AuthRequestModel {
     required this.email,
     required this.password,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'password': password,
+    };
+  }
+
+  factory AuthRequestModel.fromJson(Map<String, dynamic> json) {
+    return AuthRequestModel(
+      email: json['email'],
+      password: json['password'],
+    );
+  }
 }
--- lib/data/models/login_request_model.dart
+++ lib/data/models/login_request_model.dart
@@ -6,4 +6,18 @@ class LoginRequestModel {
     required this.emailOrPhone,
     required this.password,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'emailOrPhone': emailOrPhone,
+      'password': password,
+    };
+  }
+
+  factory LoginRequestModel.fromJson(Map<String, dynamic> json) {
+    return LoginRequestModel(
+      emailOrPhone: json['emailOrPhone'],
+      password: json['password'],
+    );
+  }
 }
